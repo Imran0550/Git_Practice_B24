ed596e32e7d77fbab08d8a23aff36be45bfa6f53
sayidimrannazari <Sayidi_n@yahoo.com>
2021-10-03 12:44:09 -0400

    added third file

---
+++ src/Third.java
@@ -0,0 +1,13 @@
+public class Third {
+
+    public void m(String s){
+
+        s= "java is fun";
+        System.out.println(s);
+
+    }
+    public static String s(String s){
+
+       return "java is hard";
+    }
+}
